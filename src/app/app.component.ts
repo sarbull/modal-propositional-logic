960d33e7670895d5487030286592312883168e1f
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-10-18 23:15:24 +0300

    First version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as MPL from '../../lib/mpl';
+import { Graph } from './Graph';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mpl';
+
+  ngOnInit() {
+    console.log(new MPL.Model());
+
+    new Graph(750, 600, '.graph');
+  }
 }
